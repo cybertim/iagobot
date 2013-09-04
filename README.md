5d6f11f9d4ab8f01e47390f03b40c85b6074b38f
Tim <cybertim@debian6.xlshosting.net>
2013-09-04 12:23:09 +0200

    initial release

--- README.md
+++ README.md
@@ -2,3 +2,27 @@ iagobot
 =======
 
 simple Whatsapp bridge to and from IRC based on irclib and yowsup
+
+### Configuration
+
+1. copy 'config.py.example' to 'config.py'
+2. edit the 'config.py' with your fav editor
+3. set all settings according to your own wishes
+4. run './bot.py'
+
+### Usage Information
+
+You can set a whatsapp 'jid' of the whatsapp group in the 'config.py' but if you invite the 'iagobot' to a groupschat it will be the default group for the running session.
+This is also an easy way to retreive the 'whatsapp group jid' from the logs and set it as a default in the 'config.py'
+
+Currently the bot can only join 1 channel and 1 groupschat.
+
+### Registration Whatsapp
+
+You can register an extra phone number (by an extra simcard) to use with this bot.
+To register to whatsapp and get the correct password:
+
+1. go to http://whitesoft.dyndns.org:2222/whatsapp_sms and request an sms
+2. clone yowsup (or use yowsup delivered with iagobot)
+3. use: ./yowsup-cli --register <code from sms>
+4. copy / paste the password from the output to the 'config.py'
---
+++ bot.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+import time
+import base64
+from irclib import IRC, ServerConnectionError
+from Yowsup.connectionmanager import YowsupConnectionManager
+from config import *
+
+ycm = YowsupConnectionManager()
+signalsInterface = ycm.getSignalsInterface()
+methodsInterface = ycm.getMethodsInterface()
+
+client = IRC()
+try:
+    irc = client.server().connect(irc_server, irc_port, irc_nickname)
+except ServerConnectionError as x:
+    print x
+    exit(1)
+
+def ircOnPubMsg(connection, event):
+    text = event.source().split('!')[0] + "> " + event.arguments()[0]
+    if wa_group == "":
+        print "set a whatsapp group first!"
+    else:
+        methodsInterface.call("message_send", (wa_group, text.encode("utf-8")))
+
+def ircOnPrivMsg(connection, event):
+    print event.arguments
+
+def ircOnConnect(connection, event):
+    irc.join(irc_channel)
+
+def ircOnDisconnect(connection, event):
+    print "irc disconnected"
+    exit(0)
+
+def waOnAuthFailed(username, reason):
+    print "Logging failed because %s" % reason
+
+def waOnAuthSuccess(username):
+    print "Logged in with %s" % username
+    methodsInterface.call("ready")
+
+def waOnMessageReceived(messageId, jid, messageContent, timestamp, wantsReceipt, pushName, isBroadCast):
+    try:
+        nick = wa_contacts[jid[:11]]
+    except KeyError:
+        nick = "unknown"
+    irc.privmsg(irc_channel, "[" + nick + "] " + messageContent)
+    methodsInterface.call("message_ack", (jid, messageId))
+
+def waOnGroupMessageReceived(messageId, jid, author, messageContent, timestamp, wantsReceipt, pushName):
+    if wa_group == "":
+        print "setting group to " + jid
+        global wa_group
+        wa_group = jid
+    methodsInterface.call("message_ack", (jid, messageId))
+
+wa_password = base64.b64decode(bytes(wa_password.encode('utf-8')))
+
+# whatsapp handlers
+
+signalsInterface.registerListener("auth_fail", waOnAuthFailed)
+signalsInterface.registerListener("auth_success", waOnAuthSuccess)
+signalsInterface.registerListener("message_received", waOnMessageReceived)
+signalsInterface.registerListener("group_messageReceived", waOnGroupMessageReceived)
+
+# irc handlers
+
+irc.add_global_handler("disconnect", ircOnDisconnect)
+irc.add_global_handler("welcome", ircOnConnect)
+irc.add_global_handler("privmsg", ircOnPrivMsg)
+irc.add_global_handler("pubmsg", ircOnPubMsg)
+
+methodsInterface.call("auth_login", (wa_username, wa_password))
+
+# main loop
+client.process_forever()
